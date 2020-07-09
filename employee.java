63f23fe80f635d6a9ae9ca3639d6a8d242afba3e
Aswini2001 <64783778+Aswini2001@users.noreply.github.com>
2020-07-09 23:20:45 +0530

    updated employee.java

--- employee.java
+++ employee.java
@@ -1,4 +1,4 @@
-package myjava;
+ package myjava;
 
 import java.util.Scanner;
 
@@ -6,15 +6,15 @@ public class Employee {
 	 String name ;
      String address;
      int year;
+     int salary;
      
 
 	public static void main(String[] args) {
-  //creatin an object of the class employee//
+		//creating object of a class employee
  Employee obj[] = new Employee[3];
 
  Scanner ss = new Scanner (System .in);
- //getting input from the user//
- 
+ // getting input from users 
  for (int i=0;i<obj.length;i++)
  {
   obj[i] = new Employee();
@@ -25,19 +25,18 @@ System.out.println("enter the employee name: ");
 	 obj[i].address = ss.nextLine();
 	 System.out.println("enter the year of joining : ");
 	 obj[i].year = ss.nextInt();
+	 System.out.println("enter the salary : ");
+	 obj[i].salary = ss.nextInt();
 	 String dummy;
 	 dummy = ss.nextLine();
  }
  System.out.println();
- 
- //printing the output//
- 
- System.out.println("Name "+"           "+"Year Of Join  "+"            "+"Address ");
+		//printing the output
+ System.out.println("Name "+"           "+"Year Of Join  "+"            "+"Salary"+"            " +" Address ");
  for (int i=0;i<obj.length;i++)
  {
-	 System.out.println(obj[i].name+"           "+obj[i].year+"           "+obj[i].address);
+	 System.out.println(obj[i].name+"           "+obj[i].year+"                "+"rs."+obj[i].salary+"           "+obj[i].address);
  }
- 
  
 }
 }
